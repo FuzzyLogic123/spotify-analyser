89e6781eb66a7b61b925ebd6745ebc08899c857b
Patrick Edwards <pedw0004@student.monash.edu>
2021-11-12 15:31:32 +1100

    styled login page

--- src/LoginHero/LoginHero.js
+++ src/LoginHero/LoginHero.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { IoIosArrowDown as Arrow } from 'react-icons/io'
 
 function LoginHero() {
     return ( 
+        <div className="heroWrapper">
+        <div className="background-circle-small"></div>
+        <div className="background-circle-medium"></div>
+        <div className="background-circle-large"></div>
         <div className="headerWrapper">
-        <h1 className="header">
-        Analyse Your Spotify<span style = {{ color: "green", margin: "0"}}> . </span>
-        </h1> 
+            <h1 className="header">
+            Analyse Your Spotify<span style = {{ color: "green", margin: "0"}}>.</span>
+            </h1> 
         </div>
+            <Arrow className="arrow"/>
+        </div> 
     );
 }
 
